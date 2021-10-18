cb560f6933464ba4b8bcc1c015c94cf8cf513b64
rawanazazi12 <rawanazazi12@gmail.com>
2021-10-18 23:33:12 +0300

    lab16+17

--- codeFellowship/src/main/java/com/codeFellows/codeFellowship/web/ApplicationController.java
+++ codeFellowship/src/main/java/com/codeFellows/codeFellowship/web/ApplicationController.java
@@ -1,6 +1,4 @@
 package com.codeFellows.codeFellowship.web;
-
-
 import com.codeFellows.codeFellowship.domain.ApplicationUser;
 import com.codeFellows.codeFellowship.infrastructure.AppUserRepository;
 import com.codeFellows.codeFellowship.infrastructure.PostRepository;
@@ -10,14 +8,18 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.security.Principal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 public class ApplicationController {
@@ -41,14 +43,37 @@ public class ApplicationController {
     }
 
     @PostMapping("/signup")
-    public RedirectView createUser(String username, String password, String dateOfBirth, String firstname, String lastname, String bio) throws ParseException {
+    public RedirectView createUser( @RequestParam (value = "username") String username,
+                                    @RequestParam (value = "password") String password,
+                                    @RequestParam (value = "dateOfBirth") String dateOfBirth,
+                                    @RequestParam (value = "firstName") String firstName,
+                                    @RequestParam (value = "lastName") String lastName,
+                                    @RequestParam (value = "bio") String bio) throws ParseException {
         Date newDate = new SimpleDateFormat("yyyy-MM-dd").parse(dateOfBirth);
-        ApplicationUser newUser = new ApplicationUser(username,encoder.encode(password), firstname, lastname,newDate, bio);
+        ApplicationUser newUser = new ApplicationUser(username,encoder.encode(password), firstName, lastName,newDate, bio);
         appUserRepository.save(newUser);
         Authentication authentication = new UsernamePasswordAuthenticationToken(newUser, null, new ArrayList<>());
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        return new RedirectView("/");
+        return new RedirectView("/login");
+    }
+
+    @GetMapping("/users")
+    public String getUsersPage(Principal principal, Model model) {
+        ApplicationUser appUser = appUserRepository.findUserByUsername(principal.getName());
+        List<ApplicationUser> users = (List<ApplicationUser>) appUserRepository.findAll();
+        model.addAttribute("appUser",appUser);
+        model.addAttribute("principal", principal.getName());
+        model.addAttribute("users", users);
+        return "users";
+    }
+
+    @GetMapping("/myProfile")
+    public String getMyProfilePage(Principal principal, Model model) {
+        ApplicationUser appUser = appUserRepository.findUserByUsername(principal.getName());
+        model.addAttribute("appUser",appUser);
+        model.addAttribute("principal", principal.getName());
+        return "myProfile";
     }
 
 
--- codeFellowship/src/main/java/com/codeFellows/codeFellowship/web/HomeController.java
+++ codeFellowship/src/main/java/com/codeFellows/codeFellowship/web/HomeController.java
@@ -1,15 +1,26 @@
 package com.codeFellows.codeFellowship.web;
 
 
+import com.codeFellows.codeFellowship.infrastructure.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String getHomePage() {
+    @Autowired
+    AppUserRepository appUserRepository;
 
+    @GetMapping("/")
+    public String getHome(Principal principal , Model model){
+        if(principal!=null){
+            model.addAttribute("username", principal.getName());
+            return "home";
+        }
         return "home";
     }
 }
